0ba0228ba71700ddb5e469b38765005f0ce94d53
Fanindra Nayak <fanindranayak@gmail.com>
2022-10-27 21:34:38 +0530

    Final Project

--- aes.py
+++ aes.py
@@ -1,32 +1,7 @@
-# importing AES
-# installing pycryptodome
 from Crypto.Cipher import AES
-
-# encryption key
-key = b'Sixteen byte key'
-text =  b'hello from other side'
-# # create new instance of cipher
-# cipher = AES.new(key, AES.MODE_EAX)
-
-# # data to be encrypted
-# # data = "Welcome to copyassignment.com!".encode()
-
-
-# # encrypt the data
-# ciphertext = cipher.encrypt(text)
-
-# # print the encrypted data
-# print("Cipher text:", ciphertext)
-
-# generate new instance with the key and nonce same as encryption cipher
-# cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-# nonce = cipher.nonce
-# cipher =  AES.new(key, AES.MODE_EAX, nonce)
-# # decrypt the data
-# plaintext = cipher.decrypt(ciphertext)
-# print("Plain text:", plaintext.decode())
-
-def enc(text,key):
+# key = b'Sixteen byte key'
+# text = b'Y MQDTUHUT BEDUBO QI Q SBEKT JXQJ VBEQJI ED.'
+def AESEncryption(text,key):
     cipher = AES.new(key, AES.MODE_EAX)
 
     # data to be encrypted
@@ -37,17 +12,17 @@ def enc(text,key):
     ciphertext = cipher.encrypt(text)
 
     # print the encrypted data
-    print("Cipher text:", ciphertext)
+    # print("Cipher text:", ciphertext)
 
     return cipher,ciphertext
 
-datec=enc(text,key)
-
-def dec(ctext,key,cipher):
+# datec=AESEncryption(text,key)
+# print(datec)
+def AESDEcryption(ctext,key,cipher):
     nonce = cipher.nonce
     cipher =  AES.new(key, AES.MODE_EAX, nonce)
     # decrypt the data
     plaintext = cipher.decrypt(ctext)
     # print("Plain text:", plaintext.decode())
-
-dec(datec[1],key,datec[0])
+    return plaintext.decode()
+# AESDEcryption(datec[1],key,datec[0])
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-# dectxt=vigenere_decrypt(dec,VignerCipherKey)
-# print(dectxt)
-
