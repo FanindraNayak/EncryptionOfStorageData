1ec34ce939825bcf4c983b56ff0e3cbc9ff1e858
Fanindra Nayak <fanindranayak@gmail.com>
2022-11-07 23:32:44 +0530

    Final Project

--- Project.py
+++ Project.py
@@ -9,81 +9,98 @@ from Vigenère import *
 from AES import *
 from DES import *
 
-# PlainText = input("Enter the plain Text: ")
-# CeaserCipherKey = input("Enter the CeaserCipherKey: ")
-# VignerCipherKey = input("Enter the CeaserCipherKey: ")
-# DesCipherKey = input("Enter the CeaserCipherKey: ")
-# AESCipherKey = input("Enter the Key: ")
-# For secret input
-# password = getpass()
+PlainText = input("Enter the plain Text: ")
+VignerCipherKey = input("Enter the VignerCipherKey: ")
+DesCipherKey1 = input("Enter the DesCipherKey: ")
+AESCipherKey2 = input("Enter the AESCipherKey: ")
+DesCipherKey=DesCipherKey1.encode()
+AESCipherKey=AESCipherKey2.encode()
 
-PlainText = "I wandered lonely as a cloud That floats on."
+# Sample Inpute
 
-VignerCipherKey = "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
+# PlainText = "I wandered lonely as a cloud That floats on."
 
-DesCipherKey = b'hello123'
+# VignerCipherKey = "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
 
-AESCipherKey = b'Sixteen byte key'
+# DesCipherKey = b'hello123'
+
+# AESCipherKey = b'Sixteen byte key'
 
 CeaserCipherKey = len(VignerCipherKey)//3
 
-# Step 1 Ceaser Cipher encryption
+def enc(PlainText,CeaserCipherKey,AESCipherKey,VignerCipherKey,DesCipherKey):
+    # Step 1 Ceaser Cipher encryption
 
-CeaserEncryptedText =  CeaserEncrypt(PlainText,CeaserCipherKey)
-print("\n")
-print("---------------  ENCRYPTION  ---------------")
-print("\n")
-print("Plain Text after Ceaser Cipher Encryption : \n")
-print(CeaserEncryptedText)
-print("\n")
+    CeaserEncryptedText =  CeaserEncrypt(PlainText,CeaserCipherKey)
+    print("\n")
+    print("---------------  ENCRYPTION  ---------------")
+    print("\n")
+    print("Plain Text after Ceaser Cipher Encryption : \n")
+    print(CeaserEncryptedText)
+    print("\n")
+    
+    # Step 2 AES encryption
 
-# Step 2 AES encryption
+    AesEncrypttedText=AESEncryption(CeaserEncryptedText.encode(),AESCipherKey)
+    print("Encrypted Plain Text obatined after AES encryption : \n")
+    print(AesEncrypttedText[1])
+    print("\n")
+    
+    # Step 3 for key Encryption using Vigenere
 
-AesEncrypttedText=AESEncryption(CeaserEncryptedText.encode(),AESCipherKey)
-print("Encrypted Plain Text obatined after AES encryption : \n")
-print(AesEncrypttedText[1])
-print("\n")
+    dec=vigenere_encrypt(AESCipherKey.decode(),VignerCipherKey)
+    print("Key after Vigenere Encryption: \n")
+    print(dec)
+    print("\n")
 
-# Step 3 for key Encryption using Vigenere
+    # Step 4 for key Encryption using DES
 
-dec=vigenere_encrypt(AESCipherKey.decode(),VignerCipherKey)
-print("Key after Vigenere Encryption: \n")
-print(dec)
-print("\n")
+    dest =DesEncrryption(dec.encode(),DesCipherKey)
+    print("Key obtained from DES Encryption: \n")
+    print(dest)
+    print("\n")
 
-# Step 4 for key Encryption using DES
+    return dest,AesEncrypttedText
 
-dest =DesEncrryption(dec.encode(),DesCipherKey)
-print("Key obtained from DES Encryption: \n")
-print(dest)
-print("\n")
 
-print("---------------  DECRYPTION  ---------------")
-print("\n")
-# Step 1 Decrypting received Plain text using AES
+def dec(AesEncrypttedText,CeaserCipherKey,DesCipherKey,dest,VignerCipherKey):
+    print("---------------  DECRYPTION  ---------------")
+    print("\n")
 
-AesDecrypttedText = AESDEcryption(AesEncrypttedText[1],AESCipherKey,AesEncrypttedText[0])
+    # Step 1 Decrypting received Plain text using AES
 
-print("Decrypting received Plain text using AES: \n")
-print(AesDecrypttedText)
-print("\n")
+    AesDecrypttedText = AESDEcryption(AesEncrypttedText[1],AESCipherKey,AesEncrypttedText[0])
 
-# Step 2 Decrypting output of AES with ceaser
+    print("Decrypting received Plain text using AES: \n")
+    print(AesDecrypttedText)
+    print("\n")
 
-CeaserDecryptedText = CeaserCipherDecrypt(AesDecrypttedText,CeaserCipherKey)
-print("Original Plain Text: \n")
-print(CeaserDecryptedText)
-print("\n")
+    # Step 2 Decrypting output of AES with ceaser
 
-# Step 3 for key Decrypt using DES
+    CeaserDecryptedText = CeaserCipherDecrypt(AesDecrypttedText,CeaserCipherKey)
+    print("Original Plain Text: \n")
+    print(CeaserDecryptedText)
+    print("\n")
 
-print("Decrypting Key using DES: \n")
-dest1 =DesDecryption(dest,DesCipherKey)
-print(dest1)
-print("\n")
+    # Step 3 for key Decrypt using DES
 
-# Step 4 for key Decryption of output of DES with vigenere
+    print("Decrypting Key using DES: \n")
+    dest1 =DesDecryption(dest,DesCipherKey)
+    print(dest1)
+    print("\n")
 
-print("Secret Key: \n")
-dectxt=vigenere_decrypt(dest1.decode(),VignerCipherKey)
-print(dectxt)
+    # Step 4 for key Decryption of output of DES with vigenere
+
+    print("Secret Key: \n")
+    dectxt=vigenere_decrypt(dest1.decode(),VignerCipherKey)
+    print(dectxt)
+
+    return dest1
+
+
+dest=enc(PlainText,CeaserCipherKey,AESCipherKey,VignerCipherKey,DesCipherKey)
+
+
+dec(dest[1],CeaserCipherKey,DesCipherKey,dest[0],VignerCipherKey)
+
+
